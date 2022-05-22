20d467df405fa0dff09bdee3adbe80870f59be03
c8ler <kotovsergey@yandex.ru>
2022-05-22 17:46:30 +0300

    dz #3

---
+++ Task_19/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 19: Напишите программу, которая принимает на
+вход пятизначное число и проверяет, является ли оно
+палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да */
+Console.WriteLine("Введите пятизначное число: ");
+int palindrom = Convert.ToInt32(Console.ReadLine());
+if (palindrom > 0 && palindrom < 100000)
+{
+    int chislo1 = (palindrom - palindrom % 10000) / 10000;
+    int chislo2 = ((palindrom - chislo1 * 10000) - palindrom % 1000) / 1000;
+    int chislo3 = ((palindrom - chislo1 * 10000 - chislo2 * 1000) - palindrom % 100) / 100;
+    int chislo4 = (((palindrom - chislo1 * 10000 - chislo2 * 1000 - chislo3 * 100)) - palindrom % 10) / 10;
+    int chislo5 = palindrom % 10;
+    if (chislo1 == chislo5 && chislo2 == chislo4) Console.WriteLine("Да, число является палиндромом");
+    else Console.WriteLine("Нет, число не является палиндромом");
+}
+else Console.WriteLine("Неверное значение");
---
+++ Task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 21: Напишите программу, которая принимает на
+вход координаты двух точек и находит расстояние между
+ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+Console.Write("Введите координату X точки A: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки A: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z точки A: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X точки B: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки B: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z точки B: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+var dlina = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));
+Console.WriteLine("Расстояние между точками равно " + string.Format("{0:N2}", dlina));
